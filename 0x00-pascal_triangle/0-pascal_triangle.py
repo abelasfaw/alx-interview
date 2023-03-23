a40a8bdc09cce3585599052ca683d5c1726e437c
abelasfaw <abeloman89@gmail.com>
2023-03-23 13:54:35 -0700

    fix over indent

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,12 +9,12 @@ def pascal_triangle(n):
     result = []
     if (n <= 0):
         return result
-    for i in range (n):
-            current = []
-            for x in range (i+1):
-                if (x == 0 or x == i):
-                    current.append(1)
-                elif (i > 0 and x > 0):
-                    current.append(result[i-1][x-1] + result[i-1][x])
-            result.append(current)
+    for i in range(n):
+        current = []
+        for x in range(i+1):
+            if (x == 0 or x == i):
+                current.append(1)
+            elif (i > 0 and x > 0):
+                current.append(result[i-1][x-1] + result[i-1][x])
+        result.append(current)
     return result
