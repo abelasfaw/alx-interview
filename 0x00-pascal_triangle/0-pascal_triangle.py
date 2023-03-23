fa0db1443d2d018bca16e79e71f69bcfb04c839f
abelasfaw <abeloman89@gmail.com>
2023-03-23 20:49:13 +0000

    added task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Pascal's triangle'''
+
+
+def pascal_triangle(n):
+    '''Returns a 2d list of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    result = []
+    if (n <= 0):
+        return result
+    for i in range (n):
+            current = []
+            for x in range (i+1):
+                if (x == 0 or x == i):
+                    current.append(1)
+                elif (i > 0 and x > 0):
+                    current.append(result[i-1][x-1] + result[i-1][x])
+            result.append(current)
+    return result
