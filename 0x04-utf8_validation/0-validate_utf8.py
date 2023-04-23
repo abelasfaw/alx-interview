60b6382fcc98660c7dcf72464455830e035292e7
abelasfaw <abeloman89@gmail.com>
2023-04-23 06:22:53 -0700

    rewrite solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,16 +2,6 @@
 '''UTF-8 Validation'''
 
 
-def startsWith10(binary_strs):
-    '''checks if binary form starts with 10'''
-    res = True
-    for element in binary_strs:
-        if (not(element.startswith('10'))):
-            res = False
-            break
-    return res
-
-
 def convertToBinary(num):
     '''converts int to binary form and returns
     the 8 list significant bits'''
@@ -23,47 +13,28 @@ def convertToBinary(num):
 
 def validUTF8(data):
     '''determines if a given data set represents a valid UTF-8 encoding'''
-    isValid = True
-    for i in range(len(data)):
-        binary_form = convertToBinary(data[i])
-        # print("bin: {}, dec: {}".format(binary_form, data[i]))
-        if (binary_form.startswith('0')):
-            continue
-        elif (binary_form.startswith('110')):
-            # print("2:byte")
-            if (i + 1 < len(data)):
-                next_byte = convertToBinary(data[i+1])
-                # print(next_byte)
-                if (startsWith10([next_byte])):
-                    i = i + 1
-                else:
-                    return False
-            else:
+    start_byte = True
+    trailing_bytes = 0
+    for num in data:
+        binary = convertToBinary(num)
+        if start_byte:
+            if binary.startswith('10'):
                 return False
-        elif (binary_form.startswith('1110')):
-            # print("3:byte")
-            if (i + 2 < len(data)):
-                next_byte1 = convertToBinary(data[i+1])
-                next_byte2 = convertToBinary(data[i+2])
-                # print(next_byte1, next_byte2)
-                if (startsWith10([next_byte1, next_byte2])):
-                    i = i + 2
-                else:
-                    return False
-            else:
+            elif binary.startswith('110'):
+                trailing_bytes = 1
+                start_byte = False
+            elif binary.startswith('1110'):
+                trailing_bytes = 2
+                start_byte = False
+            elif binary.startswith('11110'):
+                trailing_bytes = 3
+                start_byte = False
+        else:
+            if not binary.startswith('10'):
                 return False
-        elif (binary_form.startswith('11110')):
-            # print("4:byte")
-            if (i + 3 < len(data)):
-                next_byte = convertToBinary(data[i+1])
-                next_byte1 = convertToBinary(data[i+2])
-                next_byte2 = convertToBinary(data[i+3])
-                if (startsWith10([next_byte, next_byte1, next_byte2])):
-                    i = i + 3
-                else:
-                    return False
-            else:
+            trailing_bytes -= 1
+            if (trailing_bytes < 0):
                 return False
-        elif (binary_form.startswith('10')):
-            return False
-    return isValid
+            if (trailing_bytes == 0):
+                start_byte = True
+    return True
